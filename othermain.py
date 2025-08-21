a7c9eb97101655788d3ff4d725434a68c150ee61
Erick Tran <thelittlebotengineer@gmail.com>
2025-08-21 13:04:45 -0700

    feat(othermain.py): apply othermain testing code

--- othermain.py
+++ othermain.py
@@ -3,24 +3,28 @@ import moteus
 import moteus_pi3hat
 import math
 from Utils.Controller import Controller
-from Swerve.SwerveModule import SwerveModule
-from wpimath.geometry import Rotation2d
-from wpimath.kinematics import SwerveModuleState
+# from Swerve.SwerveModule import SwerveModule
+# from wpimath.geometry import Rotation2d
+# from wpimath.kinematics import SwerveModuleState
 # from Swerve.SwerveMotor import SwerveMotor
 
 
 async def main():
     transport = moteus_pi3hat.Pi3HatRouter(
         servo_bus_map = {
-            1:[11,12]
+            1:[11,12,13,14,15,16,17,18]
         }
     )
 
+    servos = {
+        servo_id : moteus.Controller(id=servo_id, transport=transport)
+        for servo_id in [11,12,13,14,15,16,17,18]
+    }
 
     controller = Controller()
-    swerve_module = SwerveModule(drive_id=11, steer_id=12, transport=transport)
+    # swerve_module = SwerveModule(drive_id=11, steer_id=12, transport=transport)
 
-    await swerve_module.stop()
+    # await swerve_module.stop()
     # Example usage of named bindings:
     #   LEFT_X, LEFT_Y: left joystick axes
     #   RIGHT_X, RIGHT_Y: right joystick axes
@@ -28,48 +32,77 @@ async def main():
     # See Controller.py for full mapping.
     while True:
         try:
-            # await transport.cycle([
-            #     servos[11].make_position(
-            #         position=math.nan,
-            #         # Use named constant for left joystick Y axis, scaled to -10..10
-            #         velocity=controller.get_axis(Controller.LEFT_Y) * 15,
-            #         accel_limit=30,
-            #         velocity_limit=15,
-            #         watchdog_timeout=5,
-            #     )
-            # ])
+            # Odd servo IDs = drive
+            # Even servo IDs = steer
+            # Order goes FL, FR, BR, BL
 
-            # await transport.cycle([
-            #     servos[12].make_position(
-            #         position=math.nan,
-            #         # Use named constant for left joystick Y axis, scaled to -10..10
-            #         velocity=controller.get_axis(Controller.LEFT_X) * 15,
-            #         accel_limit=30,
-            #         velocity_limit=15,
-            #         watchdog_timeout=5,
-            #     )
-            # ])
-            # # Example: check if A button is pressed
-            # if controller.get_button(Controller.BUTTON_A):
-            #     print("Button A pressed!")
+            # Modules that need fixing
+            # IDs: 11 (FL Drive)
+            # 14 (FR Steer)
+            # 15 (BR Drive)
+            # 18 (BL Steer)
 
-            #ttest swerve module code
-            left_x = controller.get_axis(Controller.LEFT_X)
-            left_y = controller.get_axis(Controller.LEFT_Y)
+            servo_list1 = [11,13,15,17]
+            servo_list2 = [12,14,16,18]
+            # Apply to odd servos only
+            await transport.cycle([
+                servo.make_position(
+                    position=math.nan,
+                    # Use named constant for left joystick Y axis, scaled to -10..10
+                    velocity=controller.get_axis(Controller.LEFT_Y) * 15,
+                    accel_limit=15,
+                    velocity_limit=15,
+                    watchdog_timeout=5,
+                )
+                for servo in servos.values() if servo.id in servo_list1
+            ])
+
+            await transport.cycle([
+                servo.make_position(
+                    position=math.nan,
+                    # Use named constant for left joystick Y axis, scaled to -10..10
+                    velocity=controller.get_axis(Controller.LEFT_X) * 15,
+                    accel_limit=15,
+                    velocity_limit=15,
+                    watchdog_timeout=5,
+                )
+                for servo in servos.values() if servo.id in servo_list2
+            ])
+            # Example: check if A button is pressed
+            if controller.get_button(Controller.BUTTON_A):
+                print("Button A pressed!")
+                # stop all motors
+                await transport.cycle([
+                    servo.make_position(
+                        position=math.nan,
+                        velocity=0,
+                        accel_limit=15,
+                        velocity_limit=15,
+                        watchdog_timeout=5,
+                    )
+                    for servo in servos.values()
+                ])
+                # Exit
+                exit(0)
+            
+
+            # test swerve module code
+            # left_x = controller.get_axis(Controller.LEFT_X)
+            # left_y = controller.get_axis(Controller.LEFT_Y)
 
             # use setstate and getstate methods from SwerveModule
             # Example: set the speed and angle of a swerve module
 
-            await swerve_module.setState(SwerveModuleState(left_y, Rotation2d.fromDegrees(left_x * 180)), transport)
+            # await swerve_module.setState(SwerveModuleState(left_y, Rotation2d.fromDegrees(left_x * 180)), transport)
             
 
         except KeyboardInterrupt:
             print("Program interrupted by user. Stopping all modules.")
-            await swerve_module.stop()
+            # await swerve_module.stop()
             exit(0)
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-            await swerve_module.stop()
+            # await swerve_module.stop()
     
 
 
